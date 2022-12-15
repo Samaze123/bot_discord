0330a9f72cfca1332d30e5e65304cac53242d8ed
Samaze123456 <sa.lambert@students.ephec.be>
2022-12-15 12:59:51 +0100

    Remove docstrings

--- bot.py
+++ bot.py
@@ -25,17 +25,11 @@ bot = commands.Bot(command_prefix=prefix, intents=intents)
 
 @bot.event
 async def on_ready():
-    """
-f
-    """
     print(f'{bot.user.name} has connected to Discord!')
 
 
 @bot.command(name="gif", help="Choose for you a random gif from Tenor with a search tem(default is 'excited'")
 async def random_gif(ctx, search_term: str = "Excited"):
-    """
-f
-    """
     # get random results using default locale of EN_US
     query = requests.get(f"https://g.tenor.com/v1/random?q={search_term}&key={os.getenv('TENOR_API_KEY')}&limit=1"
                          f"&locale=en_BE&contentfilter=off&ar_range=all&media_filter=basic")
@@ -45,18 +39,12 @@ f
 
 @bot.command(name="go_offline", help="quit within discord")
 async def go_offline(ctx):
-    """
-f
-    """
     await ctx.send("I'm going offline")
     sys.exit()
 
 
 @bot.event
 async def on_command_error(ctx, error):
-    """
-f
-    """
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
 
